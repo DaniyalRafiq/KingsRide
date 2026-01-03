ebf35ffc7a6cdc5ab654a2af3e213cc48126dbbd
Daniyal Rafiq <bb420f@gmail.com>
2026-01-03 07:18:36 +0500

    website done

--- src/app/translations/servicesHeroTranslations.jsx
+++ src/app/translations/servicesHeroTranslations.jsx
@@ -1,36 +1,36 @@
 export const servicesHeroTranslations = {
     English: {
         subtitle: "Up to 25%",
-        title: "off on first booking through our App",
+        title: "Off on your first booking through our App",
         description:
-            "Sagittis nunc orci posuere eget adipiscing quisque. Vitae commodo urna vitae placerat."
+            "Enjoy up to 25% off when you make your very first booking using our mobile app. Download now and start saving!"
     },
 
     Urdu: {
         subtitle: "25٪ تک",
         title: "ہماری ایپ کے ذریعے پہلی بکنگ پر رعایت",
         description:
-            "ساگیٹس نَنک اورسی پوسویئر ایجیٹ ایڈیپیسنگ کوئسکی۔ ویتے کوموڈو اُرنا ویتے پلیسریٹ۔"
+            "جب آپ ہماری موبائل ایپ کے ذریعے پہلی بار بکنگ کریں گے تو آپ کو 25٪ تک رعایت ملے گی۔ ابھی ڈاؤن لوڈ کریں اور بچت شروع کریں!"
     },
 
     German: {
         subtitle: "Bis zu 25%",
-        title: "Rabatt auf die erste Buchung über unsere App",
+        title: "Rabatt auf Ihre erste Buchung über unsere App",
         description:
-            "Sagittis nunc orci posuere eget adipiscing quisque. Vitae commodo urna vitae placerat."
+            "Genießen Sie bis zu 25% Rabatt, wenn Sie Ihre erste Buchung über unsere mobile App vornehmen. Jetzt herunterladen und sparen!"
     },
 
     French: {
-        subtitle: "Jusqu’à 25 %",
-        title: "de réduction sur la première réservation via notre application",
+        subtitle: "Jusqu’à 25%",
+        title: "de réduction sur votre première réservation via notre application",
         description:
-            "Sagittis nunc orci posuere eget adipiscing quisque. Vitae commodo urna vitae placerat."
+            "Profitez d’une réduction allant jusqu’à 25 % lorsque vous effectuez votre première réservation via notre application mobile. Téléchargez-la maintenant et économisez !"
     },
 
     Japanese: {
         subtitle: "最大25％",
         title: "アプリからの初回予約が割引",
         description:
-            "Sagittis nunc orci posuere eget adipiscing quisque. Vitae commodo urna vitae placerat."
+            "モバイルアプリから初めて予約すると、最大25％オフになります。今すぐダウンロードして、お得にご利用ください！"
     }
 };
